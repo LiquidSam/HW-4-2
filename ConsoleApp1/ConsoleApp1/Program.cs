f6f47cbcbe52f1dfe72476c278a8cf0c15b29c8d
liquidsamlol <liquidsamlol@gmail.com>
2018-10-05 01:12:01 +0300

    done

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the word");
+            string word = Convert.ToString(Console.ReadLine());
+            int x = 0;
+            char a = 'a';
+
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i]==a)
+                
+                    x++;
+                
+            }
+            Console.WriteLine("The ammount of 'A' letters is "+x);
+           
+        }
+    }
+}
